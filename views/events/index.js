5bae7a9e3cc60a8706ad0f3f283fe9461ab70f7f
jeet bafna <jeetanil97@gmail.com>
2017-09-10 23:24:15 +0530

    Edit events and my events

--- views/events/index.js
+++ views/events/index.js
@@ -92,3 +92,52 @@ exports.create = function(req, res, next){
 
 	workflow.emit('validate');
 };
+
+exports.edit = function(req, res, next){
+	req.app.db.models.Event.findById(req.params.id).exec(function(err, event){
+		if(err){
+			return next(err);
+		}
+
+		if(req.xhr){
+			res.send(event);
+		} else{
+			res.render('events/edit',{event: event});
+		}
+	});
+
+};
+
+
+exports.update = function(req, res, next){
+	var workflow = req.app.utility.workflow(req,res);
+	workflow.on('validate', function(){
+		if(!req.body.name){
+			workflow.outcome.errors.push('please enter a name');
+			return workflow.emit('response');
+		}
+		workflow.emit('updateEvent');
+	});
+	workflow.on('updateEvent',function(){
+		var fieldsToSet={
+			name: req.body.name,
+			description: req.body.description,
+			venue: req.body.venue,
+			date: req.body.date,
+			startTime: req.body.startTime,
+			endTime: req.body.endTime,
+			username: req.user.username
+		};
+		req.app.db.models.Event.findByIdAndUpdate(req.params.id, fieldsToSet, function(err, event){
+			if(err){
+				return workflow.emit('exception', err);
+			}
+			workflow.outcome.record = event;
+			req.flash('success', 'Event updated');
+			res.location('/events/show/'+req.params.id);
+		    res.redirect('/events/show/'+req.params.id);
+		});
+	});
+
+	workflow.emit('validate');
+};
